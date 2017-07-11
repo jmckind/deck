fc23106d46ab6cdde173ee59b901a95af6464db9
John McKenzie <jmckind@gmail.com>
2017-07-11 14:48:54 -0500

    Delegate error handling when headers already sent

--- src/util/app.js
+++ src/util/app.js
@@ -21,6 +21,9 @@ app.use(function (req, res, next) {
 });
 
 app.use(function (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
   console.error(err.stack);
   res.status(500).json({ error: 'Unexpected Error' });
 });
